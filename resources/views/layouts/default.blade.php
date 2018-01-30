495f0bf9b1321d65b313727cb1a109ba657bf09a
vagrant <vagrant@homestead>
2018-01-30 11:33:02 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>@yield('title', 'sample')</title>
+  <title>@yield('title', 'sample') - 大神</title>
+  <link rel="stylesheet" href="/css/app.css"
 </head>
 <body>
-  @yield('content')
+  @include('layouts._header')
+
+  <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+  </div>
 </body>
 </html>
